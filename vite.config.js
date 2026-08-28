3aa5c906706bdb177d9ff31d0fa801112b167efb
kalee <Zhuanz1@77UFO.local>
2026-08-28 12:16:49 +0800

    fix resources product links

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
+  appType: 'mpa',
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(import.meta.dirname, 'index.html'),
         products: resolve(import.meta.dirname, 'products.html'),
+        resources: resolve(import.meta.dirname, 'resources.html'),
       },
     },
   },
